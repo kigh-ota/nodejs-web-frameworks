7bdbf37cbd9b738196426c5c05b2470fa9a9782b
Kaiichiro <kigh.ota@gmail.com>
2022-12-18 02:13:27 +0900

    fastify/cookie

--- fastify/main.js
+++ fastify/main.js
@@ -1,5 +1,13 @@
 const fastify = require('fastify')({logger: true})
 
+// Plugins
+fastify.register(require('@fastify/cookie'), {
+  secret: "my-secret",
+  hook: "onRequest",
+  parseOptions:{}
+})
+
+// Routes
 fastify.register(require('./our-first-route'))
 
 fastify.listen({port: 3000}, (err, address) =>{
--- fastify/our-first-route.js
+++ fastify/our-first-route.js
@@ -5,6 +5,16 @@
  */
 async function routes(fastify, options) {
   fastify.get('/', async (request, reply) => {
+    request.log.info({foo: request.cookies.foo, baz: request.cookies.baz, bar: request.cookies.bar, unsignedBar: request.unsignCookie(request.cookies.bar)});
+    reply.setCookie("foo", "foo", {
+      domain: 'example.com',
+      path: '/'
+    })
+    .cookie('baz', 'baz') // alias for setCookie
+    .setCookie('bar', 'bar', {
+      path: '/',
+      signed: true
+    })
     return {hello: 'world'}
   })
 }
