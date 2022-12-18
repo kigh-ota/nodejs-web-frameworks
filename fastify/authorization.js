32c3e51d258270c3a4c88ed7434c003b85964a97
Kaiichiro <kigh.ota@gmail.com>
2022-12-18 23:33:53 +0900

    fastify/separate authorization.js

---
+++ fastify/authorization.js
@@ -0,0 +1,64 @@
+import fastifyCookie from '@fastify/cookie'
+import fastifySession from '@fastify/session'
+import fp from 'fastify-plugin'
+
+async function authorization(fastify, opts) {
+  fastify.register(fastifyCookie)
+  fastify.register(fastifySession, {
+    secret: '12345678901234567890123456789012',
+    cookie: { secure: false },
+    expires: 1800000
+  })
+
+  // route for login page
+  fastify.get('/login', (request, reply) => {
+    reply.type('text/html')
+    reply.send(loginPage())
+  })
+
+  // add a login route that handles the actual login
+  fastify.post('/login', (request, reply) => {
+    const { email, password } = request.body
+
+    if (password === 'abcdef') {
+      request.session.authenticated = true
+      reply.redirect('/')
+    } else {
+      reply.redirect(401, '/login')
+    }
+  });
+
+  // route for logout
+  fastify.get('/logout', (request, reply) => {
+    if (request.session.authenticated) {
+      request.destroySession((err) => {
+        if (err) {
+          reply.status(500)
+          reply.send('Internal Server Error')
+        } else {
+          reply.redirect('/')
+        }
+      })
+    } else {
+      reply.redirect('/')
+    }
+  });
+
+  function loginPage () {
+    return '<html>' +
+      '<head><title>Login</title></head>' +
+      '<body>' +
+      '<h1>Login</h1>' +
+      '<form action="http://localhost:3000/login" method="post">' +
+      '<h2>Email</h2>' +
+      '<input type="email" name="email">' +
+      '<h2>Password</h2>' +
+      '<input type="password" name="password">' +
+      '<br><br><button type="submit">Login</button>' +
+      '</form>' +
+      '</body>' +
+      '</html>'
+  }
+}
+
+export default fp(authorization, {name: 'authorization'})
--- fastify/main.js
+++ fastify/main.js
@@ -1,52 +1,17 @@
 import Fastify from 'fastify'
 import fastifyFormbody from '@fastify/formbody'
-import fastifyCookie from '@fastify/cookie'
-import fastifySession from '@fastify/session'
+import authorization from './authorization.js'
 
 const fastify = Fastify({logger: true})
 
 // Plugins
 fastify.register(fastifyFormbody)
-fastify.register(fastifyCookie)
-fastify.register(fastifySession, {
-  secret: '12345678901234567890123456789012',
-  cookie: { secure: false },
-  expires: 1800000
-})
+fastify.register(authorization)
 
 // Routes
-fastify.get('/login', (request, reply) => {
-  reply.type('text/html')
-  reply.send(loginPage())
-})
-// add a login route that handles the actual login
-fastify.post('/login', (request, reply) => {
-  const { email, password } = request.body
-
-  if (password === 'abcdef') {
-    request.session.authenticated = true
-    reply.redirect('/')
-  } else {
-    reply.redirect(401, '/login')
-  }
-});
 fastify.get('/', (request, reply) => {
   reply.type('text/html')
   reply.send(defaultPage(request.session.authenticated))
-});
-fastify.get('/logout', (request, reply) => {
-  if (request.session.authenticated) {
-    request.destroySession((err) => {
-      if (err) {
-        reply.status(500)
-        reply.send('Internal Server Error')
-      } else {
-        reply.redirect('/')
-      }
-    })
-  } else {
-    reply.redirect('/')
-  }
 });
 
 // Start
@@ -56,22 +21,6 @@ fastify.listen({port: 3000}, (err, address) =>{
     process.exit(1)
   }
 })
-
-function loginPage () {
-  return '<html>' +
-    '<head><title>Login</title></head>' +
-    '<body>' +
-    '<h1>Login</h1>' +
-    '<form action="http://localhost:3000/login" method="post">' +
-    '<h2>Email</h2>' +
-    '<input type="email" name="email">' +
-    '<h2>Password</h2>' +
-    '<input type="password" name="password">' +
-    '<br><br><button type="submit">Login</button>' +
-    '</form>' +
-    '</body>' +
-    '</html>'
-}
 
 function defaultPage (isAuthenticated) {
   if (isAuthenticated) {
